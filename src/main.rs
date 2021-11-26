1dd4f537980f16fd73680fe787dcaf7202add3f9
Jasper De Moor <jasperdemoor@gmail.com>
2021-11-26 17:13:47 +0100

    parse npm manifest

--- src/main.rs
+++ src/main.rs
@@ -28,13 +28,17 @@ async fn versions(path: web::Path<String>) -> impl Responder {
 async fn main() -> std::io::Result<()> {
     env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
 
+    let server_address = "127.0.0.1:8080";
+
+    println!("Starting server on {}", server_address);
+
     HttpServer::new(|| {
         App::new()
             .wrap(Logger::new("\"%r\" %s %Dms"))
             .service(package)
             .service(versions)
     })
-    .bind("127.0.0.1:8080")?
+    .bind(server_address)?
     .run()
     .await
 }
--- src/npm.rs
+++ src/npm.rs
@@ -4,13 +4,26 @@ use crate::app_error::ServerError;
 use serde::{self, Deserialize, Serialize};
 
 #[derive(Debug, Deserialize, Serialize)]
+pub struct PackageDist {
+    tarball: String,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct PackageJSON {
+    dist: PackageDist,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
 pub struct PackageManifest {
     name: String,
     #[serde(rename = "dist-tags")]
     dist_tags: HashMap<String, String>,
+    versions: HashMap<String, PackageJSON>,
 }
 
-pub async fn download_package_manifest(package_name: String) -> Result<(), ServerError> {
+pub async fn download_package_manifest(
+    package_name: String,
+) -> Result<PackageManifest, ServerError> {
     let manifest: PackageManifest =
         reqwest::get(format!("https://registry.npmjs.org/{}", package_name))
             .await?
@@ -19,5 +32,5 @@ pub async fn download_package_manifest(package_name: String) -> Result<(), Serve
 
     println!("{:?}", manifest);
 
-    Ok(())
+    Ok(manifest)
 }
