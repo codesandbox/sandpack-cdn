7ad2e621ce0feacc2d992b307c69396081e9f92c
Zeh Fernandes <ozehfernandes@gmail.com>
2022-03-23 15:25:02 +0000

    Update test_utils.rs

--- src/utils/test_utils.rs
+++ src/utils/test_utils.rs
@@ -8,3 +8,10 @@ pub fn read_fixture(fixture_name: &str) -> Result<String, ServerError> {
     let fixture_content: String = fs::read_to_string(fixture_path)?;
     Ok(fixture_content)
 }
+
+
+
+
+
+
+
