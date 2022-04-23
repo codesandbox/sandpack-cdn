69da69abfba0889ca27af334060115503ef8a2e9
Jasper De Moor <jasperdemoor@gmail.com>
2022-04-23 12:00:41 +0200

    remove compression and cors and add tracing

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
-use actix_cors::Cors;
-use actix_web::middleware::{Compress, Logger};
+use actix_web::middleware::Logger;
 use actix_web::{
     get,
-    http::header::{CacheControl, CacheDirective, ContentEncoding},
+    http::header::{CacheControl, CacheDirective},
     http::StatusCode,
     web, App, HttpResponse, HttpServer, Responder,
 };
@@ -15,6 +14,10 @@ use regex::Regex;
 use serde::{self, Deserialize, Serialize};
 use std::collections::HashMap;
 use std::env;
+use tracing_honeycomb::new_honeycomb_telemetry_layer;
+use tracing_subscriber::filter::LevelFilter;
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::registry;
 
 mod app_error;
 mod cache;
@@ -159,6 +162,25 @@ async fn versions_req_handler(
 
 #[actix_web::main]
 async fn main() -> Result<(), std::io::Error> {
+    let honeycomb_config = libhoney::Config {
+        options: libhoney::client::Options {
+            api_key: "6TRdsEEi4WhE4pbcQIGYdA".to_string(),
+            dataset: "sandpack-cdn".to_string(),
+            ..libhoney::client::Options::default()
+        },
+        transmission_options: libhoney::transmission::Options::default(),
+    };
+
+    let telemetry_layer = new_honeycomb_telemetry_layer("my-service-name", honeycomb_config);
+
+    // NOTE: the underlying subscriber MUST be the Registry subscriber
+    let subscriber = registry::Registry::default() // provide underlying span data store
+        .with(LevelFilter::INFO) // filter out low-level debug tracing (eg tokio executor)
+        .with(tracing_subscriber::fmt::Layer::default()) // log to stdout
+        .with(telemetry_layer); // publish to honeycomb backend
+
+    tracing::subscriber::set_global_default(subscriber).expect("setting global default failed");
+
     env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
 
     // TODO: Calculate cache size dynamically based on available memory?
@@ -182,18 +204,10 @@ async fn main() -> Result<(), std::io::Error> {
     let server_address = format!("0.0.0.0:{}", port);
     println!("Starting server on {}", server_address);
     HttpServer::new(move || {
-        let mut cors = Cors::default();
-        cors = cors.allow_any_header();
-        cors = cors.allow_any_method();
-        cors = cors.allow_any_origin();
-
         App::new()
             .app_data(web::Data::new(data.clone()))
             .app_data(web::Data::new(layered_cache.clone()))
-            .wrap(cors)
             .wrap(Logger::new("\"%r\" %s %Dms"))
-            // TODO: Remove this and let cloudflare handle encoding?
-            .wrap(Compress::new(ContentEncoding::Auto))
             .service(package_req_handler)
             .service(versions_req_handler)
     })
