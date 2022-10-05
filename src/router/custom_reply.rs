bd6f1b99162be546009914cd3779462c2cac10c9
Jasper De Moor <jasperdemoor@gmail.com>
2022-10-05 21:45:29 +0200

    use content-type application/javascript as most content of the msgpack is javascript strings

--- src/router/custom_reply.rs
+++ src/router/custom_reply.rs
@@ -43,7 +43,7 @@ impl CustomReply {
         // It should really be application/msgpack but
         // this is a hack to get cloudflare to encode it
         // using gzip/brotli
-        reply.add_header("content-type", "application/wasm");
+        reply.add_header("content-type", "application/javascript");
         Ok(reply)
     }
 
