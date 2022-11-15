085ebc19acc48bb09144a2310a1a6b9bba901f75
Jasper De Moor <jasperdemoor@gmail.com>
2022-11-15 10:41:57 +0100

    done timeout npm metadata, this endpoint is extremely slow on npmjs

--- src/npm/package_data.rs
+++ src/npm/package_data.rs
@@ -149,14 +149,10 @@ pub struct PackageDataFetcher {
 
 impl PackageDataFetcher {
     pub fn new() -> PackageDataFetcher {
-        let ttl = Duration::from_secs(86400);
-        let max_capacity = 250;
+        let max_capacity = 500;
         PackageDataFetcher {
             client: Arc::new(request::get_client(30)),
-            cache: Cache::builder()
-                .max_capacity(max_capacity)
-                .time_to_idle(ttl)
-                .build(),
+            cache: Cache::builder().max_capacity(max_capacity).build(),
             refresh_interval: Duration::from_secs(900),
         }
     }
