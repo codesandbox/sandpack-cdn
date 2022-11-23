fc0159cfd423e2bb9137225e4eb77f38e7eeee11
Jasper De Moor <jasperdemoor@gmail.com>
2022-11-23 13:47:43 +0100

    skip empty packages

--- src/npm_replicator/replication_task.rs
+++ src/npm_replicator/replication_task.rs
@@ -21,6 +21,13 @@ async fn sync(db_path: String) -> AppResult<()> {
                             db.delete_package(&evt.id)?;
                             println!("Deleted package {}", evt.id);
                         } else if let Some(doc) = evt.doc {
+                            // Skip empty packages
+                            if let Some(versions) = &doc.versions {
+                                if versions.len() <= 0 {
+                                    continue;
+                                }
+                            }
+
                             db.write_package(MinimalPackageData::from_doc(doc.clone()))?;
                             println!("Wrote package {} to db", evt.id);
                         }
