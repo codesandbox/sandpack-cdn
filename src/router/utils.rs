ca49ee29e5c87bc0d8f6934cfdec37ab419a2064
Jasper De Moor <jasperdemoor@gmail.com>
2023-02-15 16:56:23 +0100

    chore: update all deps

--- src/router/utils.rs
+++ src/router/utils.rs
@@ -1,10 +1,9 @@
 use crate::app_error::ServerError;
 
 pub fn decode_base64(part: &str) -> Result<String, ServerError> {
-    let decoded = base64_simd::Base64::STANDARD
-        .decode_to_boxed_bytes(part.as_bytes())
+    let decoded = base64_simd::STANDARD
+        .decode_to_vec(part.as_bytes())
         .map_err(|_e| ServerError::Base64DecodingError())?;
-    let val =
-        String::from_utf8(decoded.to_vec()).map_err(|_e| ServerError::Base64DecodingError())?;
+    let val = String::from_utf8(decoded).map_err(|_e| ServerError::Base64DecodingError())?;
     Ok(val)
 }
